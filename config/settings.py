01b5ccaa5f75a05f68d06392ad722bfd56baa124
Oybek-M <muxtaraliyevoybek@gmail.com>
2026-06-05 10:21:02 +0500

    chore: migrate database from SQLite to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'sinomed_db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1234'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
